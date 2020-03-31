08b74735ff6d5e1a6011743985d6ea1dfa41b417
Derek Kwok <derek.m.kwok@gmail.com>
2020-03-31 01:33:41 -0400

    added react-app and created initial server files

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+// process.env variables for private variables
+// require('dotenv').config();
+
+// middleware
+const cors = require('cors');  // cross origin resource sharing to allow for different origins to communicate
+// const bodyParser = require('body-parser');  // parses requests into usable format
+const helmet = require('helmet');  // security
+const morgan = require('morgan');  // logging
+
+const app = express();
+const frontendPort = 3000;
+
+app.use(cors({ origin: `http://localhost:${frontendPort}` }));
+// app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+const backendPort = 5000;
+
+app.listen(5000, () => console.log(`App running on port ${backendPort} - http://localhost:5000`));
