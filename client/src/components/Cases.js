1780f7b376d0ee841de682f6c03087718fa22c2d
Derek Kwok <derek.m.kwok@gmail.com>
2020-04-03 22:32:07 -0400

    updated cases page

--- client/src/components/Cases.js
+++ client/src/components/Cases.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 // Font-Awesome imports
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -61,32 +62,80 @@ const useStyles = makeStyles((theme) => ({
 export default function Cases() {
   const classes = useStyles();
 
-  // hooks
-  const [country, setCountry] = useState('');
-  const [cases, setCases] = useState(0);
-  const [deaths, setDeaths] = useState(0);
-  const [recovered, setRecovered] = useState(0);
-  const [active, setActive] = useState(0);
-  const [today, setToday] = useState(0);
+  // worldwide hooks
+  const [allCases, setAllCases] = useState(-1);
+  const [allDeaths, setAllDeaths] = useState(-1);
+  const [allRecovered, setAllRecovered] = useState(-1);
+  const [allActive, setAllActive] = useState(-1);
+  const [allCountries, setAllCountries] = useState(-1);
 
-  // TODO RENDER THE DATA ON THE PAGE
+  // country hooks
+  const [country, setCountry] = useState('');
+  const [cases, setCases] = useState(-1);  // -1 for initial state
+  const [deaths, setDeaths] = useState(-1);
+  const [recovered, setRecovered] = useState(-1);
+  const [active, setActive] = useState(-1);
+  const [today, setToday] = useState(-1);
+  const [todayDeaths, setTodayDeaths] = useState(-1);
+  const [critical, setCritical] = useState(-1);
+  const [loaded, setLoaded] = useState(false);
+
+  // initial render
   useEffect(() => {
     // fetch(`${root}/all`)
-    fetch(`http://localhost:5000/country/${country}`)
+    fetch(`http://localhost:5000/all`)
       .then(response => response.json())
       .then(data => {
+        setAllCases(data['cases']);
+        setAllDeaths(data['deaths']);
+        setAllRecovered(data['recovered']);
+        setAllActive(data['active']);
+        setAllCountries(data['affectedCountries']);
+      })
+      .catch(err => console.log('Error fetching data'));
+  }, []);
+
+  // re-rendering for every country change
+  useEffect(() => {
+    // fetch(`${root}/all`)
+    let query;
+    if (country.toLowerCase() === 'world') {
+      query = 'all';
+    } else {
+      query = 'country' + '/' + country;
+    }
+    fetch(`http://localhost:5000/${query}`)
+    .then(response => response.json())
+    .then(data => {
+      // if country data exists, set all values
+      if (!(data['cases'] === undefined || data['cases'] === null)) {
         setCases(data['cases']);
         setDeaths(data['deaths']);
         setRecovered(data['recovered']);
         setActive(data['active']);
         setToday(data['todayCases']);
-      })
-      .then(() => console.log(cases))
-      .catch(err => console.log('Error fetching data'));
-  });
+        setTodayDeaths(data['todayDeaths']);
+        setCritical(data['critical']);
+        setLoaded(true);
+      } else {
+        setLoaded(false);
+      }
+    })
+    .then(() => {
+      console.log(cases);
+    })
+    .catch(err => console.log('Error fetching data'));
+  }, [country]);
 
   const handleChange = (event) => {
-    setCountry(event.target.value); // setCountry is asnyc and triggers re-render, use useEffect for render changes
+    // setCases(-1);
+    // setDeaths(-1);
+    // setRecovered(-1);
+    // setActive(-1);
+    // setToday(-1);
+    // setTodayDeaths(-1);
+    // setCritical(-1);
+    setCountry(event.target.value); // setCountry is asnyc, triggers re-render, use useEffect for render changes
   };
 
   return (
@@ -99,31 +148,60 @@ export default function Cases() {
           <FontAwesomeIcon icon={faChartLine} size='1x' style={{ marginLeft: '20px' }}></FontAwesomeIcon>
         </Typography>
         <br></br>
-        <Typography paragraph variant='h5' style={{color: '#FFF'}}>
+        <Typography paragraph variant='h6' style={{color: '#FFF'}}>
           Enter the name of a country below to get more information
         </Typography>
         <form className={classes.root} noValidate autoComplete="off">
-        <TextField 
-          id="outlined-basic" 
-          label="Country" 
-          onChange={handleChange} 
-          variant="outlined" 
-          InputLabelProps={{
-            classes: {
-              root: classes.label,
-              focused: classes.focus,
-            },
-          }}
-          InputProps={{
-            style: {color: '#FFF'},
-            classes: {
-              root: classes.outlinedInput,
-              focused: classes.focus,
-              notchedOutline: classes.notchedOutline
-            }
-          }} 
-        />
-    </form>
+          <TextField 
+            id="outlined-basic" 
+            label="Country" 
+            onChange={handleChange} 
+            variant="outlined" 
+            InputLabelProps={{
+              classes: {
+                root: classes.label,
+                focused: classes.focus,
+              },
+            }}
+            InputProps={{
+              style: {color: '#FFF'},
+              classes: {
+                root: classes.outlinedInput,
+                focused: classes.focus,
+                notchedOutline: classes.notchedOutline
+              }
+            }} 
+          />
+          { !loaded ? <CircularProgress style={{ marginLeft: '25px', marginTop: '6px'}} /> : ''}
+        </form>
+        <br></br>
+        { loaded ? 
+          <React.Fragment>
+            <Typography paragraph variant='h4' style={{color: '#6495ED'}}>
+              Total Cases: {cases}
+            </Typography>
+            <Typography paragraph variant='h4' style={{color: 'red'}}>
+              Deaths: {deaths}
+            </Typography>
+            <Typography paragraph variant='h4' style={{color: 'green'}}>
+              Recovered: {recovered}
+            </Typography>
+            <Typography paragraph variant='h4' style={{color: 'yellow'}}>
+              Active: {active}
+            </Typography>
+            <Typography paragraph variant='h4' style={{color: 'yellow'}}>
+              Cases Today: {today}
+            </Typography>
+            <Typography paragraph variant='h4' style={{color: 'yellow'}}>
+              Deaths Today: {todayDeaths}
+            </Typography>
+            <Typography paragraph variant='h4' style={{color: 'yellow'}}>
+              Critical Condition: {critical}
+            </Typography>
+          </React.Fragment>
+          :
+          ''
+        }
       </main>
     </div>
   );
