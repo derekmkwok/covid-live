b2cf8137ebdd4b9dc4b21345418d3b248c2ca960
Derek Kwok <derek.m.kwok@gmail.com>
2020-04-14 03:11:39 -0400

    react build

---
+++ server/build/precache-manifest.d762f2ad5f9e3d8cb230a7089bac7ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9edb0a3fd01fb682e5015210573daaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12be6ff0e8c5e8a58d66",
+    "url": "/static/css/main.e58af2bd.chunk.css"
+  },
+  {
+    "revision": "46f99c34e859e20c5d70",
+    "url": "/static/js/2.9642bf08.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.9642bf08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12be6ff0e8c5e8a58d66",
+    "url": "/static/js/main.edc8cea5.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // process.env variables for private variables
 // require('dotenv').config();
@@ -19,12 +20,17 @@ app.use(cors({ origin: `http://localhost:${frontendPort}` }));  // cross origin 
 app.use(helmet());  // security for headers
 app.use(morgan('combined'));  // logging
 
+// react build
+app.use(express.static(path.join(__dirname, 'build')));
+
 // api routes
 app.get('/all', data.getAll);
 app.get('/country/all', data.getAllCountries);  // need this line above otherwise 'all' is considered a parameter
 app.get('/country/:country', data.getCountry);
 app.get('/time', time.getTime);
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const backendPort = process.env.PORT || 5000;
 
